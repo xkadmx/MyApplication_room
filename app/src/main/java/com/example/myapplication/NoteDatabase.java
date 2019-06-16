b755b322a68520ecc10b94cf97435cca5be180a6
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-16 20:06:25 +0200

    created method bodies for methods insert/update/delete and LiveData list GetAllUpdates in NoteRepository

--- app/src/main/java/com/example/myapplication/NoteDatabase.java
+++ app/src/main/java/com/example/myapplication/NoteDatabase.java
@@ -1,6 +1,25 @@
 package com.example.myapplication;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance; // because we need to turn the class into singleton (single instance of database
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
