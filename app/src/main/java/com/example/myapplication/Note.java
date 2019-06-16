713887ac3715f477ff11abd2d15f2f01f5b2fc77
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-16 13:29:14 +0200

    creating getters with rightclick Generate>Getters

--- app/src/main/java/com/example/myapplication/Note.java
+++ app/src/main/java/com/example/myapplication/Note.java
@@ -1,7 +1,29 @@
 package com.example.myapplication;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "note_table")
 public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
