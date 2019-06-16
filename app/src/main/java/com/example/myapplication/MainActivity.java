41e4016dcac3812d0e0f3b03e47b9669061b8cff
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-16 22:18:03 +0200

    implementing ViewModel to MainActivity class

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // providing already existing Viewmodel
     }
 }
