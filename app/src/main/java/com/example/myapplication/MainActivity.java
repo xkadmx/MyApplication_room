395f5cefde33edbaa77c1d18072fd6945edacc2e
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-17 12:17:02 +0200

    adding add_note_menu menu resource file creating AddNoteActivity as an empty Activity resource and previously added 3 icons save/add/delete

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,13 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         // providing already existing Viewmodel
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class); // we pass this this class because we want to get the instance of this ViewModel
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
-            public void onChanged(List<Note> notes) {
-                // update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            public void onChanged(@Nullable List<Note> notes) { // anytime note changes adapter should be updated as well
+                adapter.setNotes(notes);
             }
         });
     }
